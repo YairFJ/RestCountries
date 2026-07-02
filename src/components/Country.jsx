c4208b40d430e7ea6e0f148fa663d2d1dc67c562
Yali <farinayair1@gmail.com>
2026-07-02 20:35:24 -0300

    Search Engine functionality finished

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,9 +1,16 @@
-const Country = ({list}) => (
+const Country = ({list}) => {
+
+  return list.length > 10 ? (
+    <p>Too many matches, specify another filter</p>
+  ) : (
     <ul>
-        {list.map((country) => (
-            <li key={country.name.common}><p> {country.name.common} | {country.capital} </p></li>
-        ))}
+      {list.map((country) => (
+        <li key={country.name.common}>
+          <p>{country.name.common} | {country.capital}</p>
+        </li>
+      ))}
     </ul>
-)
+  )
+}
 
 export default Country
--- src/services/contries.js
+++ src/services/contries.js
@@ -5,8 +5,12 @@ const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
 
 const getAll = () => {
     const request = axios.get(`${baseUrl}/all/`)
-
     return request.then(response => response.data)
 }
 
-export default {getAll}
+const get = (country) => {
+    const request = axios.get(`${baseUrl}/name/${country}`)
+    return request.then(response => response.data)
+}
+
+export default {getAll, get}
