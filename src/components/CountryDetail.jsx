2ede346896a7126596275417d938efd32e52e0d3
Enzo Lanati <enzolanaticara@gmail.com>
2026-07-13 20:51:26 -0300

    Agrego de icon

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -6,7 +6,7 @@ const CountryDetail = ({countryselected, weather})=>{
     }
     
     const country = countryselected[0]
-
+    
     return(
         <>
         <ul>
@@ -20,9 +20,18 @@ const CountryDetail = ({countryselected, weather})=>{
                     alt={country.flags.alt}
                     width="200"    
                 />
-                <h2>Weather in {country.capital}</h2>
+                {weather && (
+                <>
+                    <h3>Temperature: {weather.main.temp} °C</h3>
 
-                <h3>Weather: {weather?.main?.temp}</h3>
+                    <img
+                    src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                    alt={weather.weather[0].description}
+                    />
+
+                    <p>Wind: {weather.wind.speed} m/s</p>
+                </>
+                )}
                 
             
                 
